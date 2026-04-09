37156d0c64074c189326932fda0b595b00f1bfd2
MatLan8 <152216691+MatLan8@users.noreply.github.com>
2026-04-10 00:49:39 +0300

    setup native libraries

--- src/screens/StatsScreen/StatsScreen.tsx
+++ src/screens/StatsScreen/StatsScreen.tsx
@@ -1,11 +1,20 @@
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { CircleLoadingIndicator } from "../../reactitcx_Components/CircleLoader/circle-loader";
 
 const StatsScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={{ color: "white" }}>StatsScreen</Text>
+      <Text style={{ color: "white" }}>Stats Screen</Text>
+      <CircleLoadingIndicator
+        dotSpacing={8}
+        dotColor="#fff"
+        style={{
+          marginTop: 60,
+        }}
+        duration={500}
+      />
     </SafeAreaView>
   );
 };
